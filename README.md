6ae218e15b0abb33df1000867a34c348885667ab
Lee Lacey <leelacey128@gmail.com>
2024-02-29 08:38:20 -0600

    Added SpringBoot

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## GameGO
 
-This is a Gaming Store MVC project.
+This is a Gaming Store MVC Spring project.
 
 The goal is to have CRUD functionality and implement separation of concerns using MVC as the design method.
---
+++ gamego/src/main/java/com/ll/gamego/gamego/GamegoApplication.java
@@ -0,0 +1,13 @@
+package com.ll.gamego.gamego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamegoApplication.class, args);
+	}
+
+}

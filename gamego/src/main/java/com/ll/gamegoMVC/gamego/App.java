54da519ad7f7a0b0057811aaf0843e7618adbad6
Lee Lacey <leelacey128@gmail.com>
2024-03-25 12:39:58 -0500

    Adds Rest dependency in pom.xml

--- gamego/src/main/java/com/ll/gamegoMVC/gamego/App.java
+++ gamego/src/main/java/com/ll/gamegoMVC/gamego/App.java
@@ -3,8 +3,6 @@ package com.ll.gamegoMVC.gamego;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.ll.gamegoMVC.gamego.ui.UserIOConsoleImpl;
-import com.ll.gamegoMVC.gamego.ui.UserIO;
 
 @SpringBootApplication
 public class App {

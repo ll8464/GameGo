1fbd8f429f38b8039ada40595df7310b92526fc6
Lee Lacey <leelacey128@gmail.com>
2024-03-25 16:34:47 -0500

    Fixed initialization error by adding ComponentScan to App file

--- gamego/src/main/java/com/ll/gamegoMVC/App.java
+++ gamego/src/main/java/com/ll/gamegoMVC/App.java
@@ -1,10 +1,12 @@
-package com.ll.gamegoMVC.gamego;
+package com.ll.gamegoMVC;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@ComponentScan("com.ll.gamegoMVC.gamego.repository")
 public class App {
 
 	public static void main(String[] args) {
--- gamego/src/main/java/com/ll/gamegoMVC/gamego/repository/GameRepository.java
+++ gamego/src/main/java/com/ll/gamegoMVC/gamego/repository/GameRepository.java
@@ -1,9 +1,12 @@
 package com.ll.gamegoMVC.gamego.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import com.ll.gamegoMVC.gamego.dao.GameDAO;
-
+@Repository
+@Component
 public interface GameRepository extends CrudRepository<GameDAO, String> {
     
 }

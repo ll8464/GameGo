ceb232d4b170cb189b236526c17b33b8de876950
Lee Lacey <leelacey128@gmail.com>
2024-03-28 11:49:47 -0500

    Adds Controller, Completes CRUD for Console

--- gamego/src/main/java/com/ll/gamegoMVC/gamego/controller/ConsoleController.java
+++ gamego/src/main/java/com/ll/gamegoMVC/gamego/controller/ConsoleController.java
@@ -1,8 +1,50 @@
 package com.ll.gamegoMVC.gamego.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ll.gamegoMVC.gamego.dao.ConsoleDAO;
+import com.ll.gamegoMVC.gamego.service.ConsoleService;
 
 @RestController
 public class ConsoleController {
+     @Autowired
+    private ConsoleService consoleService;
+
+    //add, update, get, delete
+    @GetMapping("console/{id}")      
+    public ConsoleDAO getConsole(@PathVariable String id){
+        ConsoleDAO console = consoleService.getConsoleId(id);
+        return console;
+    }
+
+    @RequestMapping("console") 
+    public List<ConsoleDAO> getAllConsoles() {
+        return consoleService.getAllConsoles();
+    }
     
+    @PostMapping("console")
+    public void addConsole(@RequestBody ConsoleDAO console) {                
+        consoleService.addConsole(console);
+    }
+    
+    @PutMapping("console/{id}")
+    public void updateConsole(@PathVariable String id, @RequestBody ConsoleDAO console) {        
+        consoleService.updateConsole(id,console);
+        
+    }
+
+    @DeleteMapping("console/{id}")
+    public void delete(@PathVariable String id){
+        consoleService.removeConsole(id);
+    }
 }
